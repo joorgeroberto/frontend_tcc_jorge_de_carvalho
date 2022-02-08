edf2b517ae8f1fa6a0349cf96de77d0407702246
Jorge Roberto <jorgercj@dcomp.ufs.br>
2022-02-07 22:19:52 -0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 100,
+};
