fa0c7730c2196ddc04a05c5ce684df2823a870f2
Jorge Roberto <jorgercj@dcomp.ufs.br>
2022-02-14 20:03:20 -0300

    feat: adding styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import styled from 'styled-components/native';
 import { View } from 'react-native';
 
-const App = () => <View />;
+const App = () => <Container />;
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: red;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default App;
