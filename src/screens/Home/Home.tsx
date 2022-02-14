9de8d2d2515872df1f2a1a882fa32eebfc6466b2
Jorge Roberto <jorgercj@dcomp.ufs.br>
2022-02-14 20:23:28 -0300

    config: adding path-alias

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export function Home() {
+  return <Container />;
+}
+
+const Container = styled.View`
+  flex: 1;
+  background-color: blue;
+  align-items: center;
+  justify-content: center;
+`;
