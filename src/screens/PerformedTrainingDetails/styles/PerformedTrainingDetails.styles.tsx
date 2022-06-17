917ea46c1a762141e98a108b1a2c368be2ed6243
Jorge Roberto <jorgercj@dcomp.ufs.br>
2022-06-17 15:57:32 -0300

    feat: PerformedTrainingDetails

--- src/screens/PerformedTrainingDetails/styles/PerformedTrainingDetails.styles.tsx
+++ src/screens/PerformedTrainingDetails/styles/PerformedTrainingDetails.styles.tsx
@@ -2,6 +2,7 @@ import colors from '@config/colors';
 import styled from 'styled-components/native';
 import { Loader as RawLoader, Header as RawHeader } from '@components/index';
 import { Dimensions } from 'react-native';
+import { boolean } from 'yup/lib/locale';
 const windowWidth = Dimensions.get('window').width;
 
 export const Container = styled.View`
@@ -12,3 +13,62 @@ export const Container = styled.View`
 `;
 
 export const Header = styled(RawHeader)``;
+
+export const ResultsText = styled.Text`
+  width: 100%;
+  text-align: center;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  color: ${colors.PRIMARY};
+`;
+
+export const StyledScrollView = styled.ScrollView`
+  flex: 1;
+  width: 100%;
+  margin-top: 21px;
+  background-color: ${colors.WHITE};
+`;
+
+interface ResultsRowContainerProps {
+  isLastContainer?: boolean;
+}
+
+export const ResultsRowContainer = styled.View<ResultsRowContainerProps>`
+  flex-direction: row;
+  height: 101px;
+  width: ${windowWidth - 42}px;
+  margin-left: 21px;
+  background-color: ${colors.WHITE};
+  border-bottom-width: 1px;
+  border-bottom-color: ${({ isLastContainer }) =>
+    isLastContainer ? colors.TRANSPARENT : colors.GRAY2};
+`;
+
+interface TrainingDataContainerProps {
+  showRightBorder?: boolean;
+}
+
+export const TrainingDataContainer = styled.View<TrainingDataContainerProps>`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  border-right-width: ${({ showRightBorder }) => (showRightBorder ? 1 : 0)}px;
+  border-right-color: ${({ showRightBorder }) =>
+    showRightBorder ? colors.GRAY2 : colors.TRANSPARENT};
+`;
+
+export const TrainingData = styled.Text`
+  font-weight: 700;
+  font-size: 35px;
+  line-height: 41px;
+  color: ${colors.PRIMARY};
+`;
+
+export const TrainingDataDescription = styled.Text`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  margin-top: 3px;
+  color: ${colors.PRIMARY3};
+`;
--- src/screens/PerformedTrainingDetails/view/PerformedTrainingDetails.tsx
+++ src/screens/PerformedTrainingDetails/view/PerformedTrainingDetails.tsx
@@ -3,33 +3,100 @@ import React, { useMemo } from 'react';
 import {
   Container,
   Header,
-  StyledFlatList,
-  EmptyListContainer,
-  EmptyListImage,
-  EmptyListText,
+  ResultsText,
+  StyledScrollView,
+  ResultsRowContainer,
+  TrainingDataContainer,
+  TrainingData,
+  TrainingDataDescription,
 } from '../styles/PerformedTrainingDetails.styles';
 import { TrainingCell } from '@components/TrainingCell';
+import { string } from 'yup';
 
 interface Props {
   route?: {
     params?: {
-      training?: TrainingData;
+      performedTraining?: PerformedTraining;
     };
   };
 }
 
-interface RenderItemProps {
-  id: string;
-  date: string;
-  onPress: () => void;
+interface RenderTrainingDataProps {
+  description: string;
+  data: string | number;
+  position?: 'left' | 'right';
 }
 
 export function PerformedTrainingDetails({ route }: Props) {
   const navigation = useNavigation();
 
+  const performedTraining = useMemo(() => {
+    return route?.params?.performedTraining as PerformedTraining;
+  }, [route]);
+
+  const renderTrainingData = ({ description, data, position }: RenderTrainingDataProps) => {
+    return (
+      <TrainingDataContainer showRightBorder={position === 'left'}>
+        <TrainingData>{data}</TrainingData>
+        <TrainingDataDescription>{description}</TrainingDataDescription>
+      </TrainingDataContainer>
+    );
+  };
+
   return (
     <Container>
       <Header title={'Treino realizado'} onPressBackButton={() => navigation.goBack()} />
+      <ResultsText>Resultados obtidos:</ResultsText>
+      <StyledScrollView>
+        <ResultsRowContainer>
+          {renderTrainingData({
+            description: 'FCmax (Bpm)',
+            data: performedTraining.fcMax,
+            position: 'left',
+          })}
+          {renderTrainingData({
+            description: 'FC Repouso (Bpm)',
+            data: performedTraining.fcRest,
+            position: 'right',
+          })}
+        </ResultsRowContainer>
+        <ResultsRowContainer>
+          {renderTrainingData({
+            description: 'FCmédia (Bpm)',
+            data: performedTraining.fcMed,
+            position: 'left',
+          })}
+          {renderTrainingData({
+            description: 'Vmax (Km/h)',
+            data: performedTraining.vMax,
+            position: 'right',
+          })}
+        </ResultsRowContainer>
+        <ResultsRowContainer>
+          {renderTrainingData({
+            description: 'Vmédia (Km/h)',
+            data: performedTraining.vMed,
+            position: 'left',
+          })}
+          {renderTrainingData({
+            description: 'Tempo',
+            data: performedTraining.duration,
+            position: 'right',
+          })}
+        </ResultsRowContainer>
+        <ResultsRowContainer>
+          {renderTrainingData({
+            description: 'Distância (Km)',
+            data: performedTraining.distance,
+            position: 'left',
+          })}
+          {renderTrainingData({
+            description: 'Calorias (Kcal)',
+            data: performedTraining.calories,
+            position: 'right',
+          })}
+        </ResultsRowContainer>
+      </StyledScrollView>
     </Container>
   );
 }
